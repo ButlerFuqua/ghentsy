739072e4844c1ec43c2505e4e99729a3cfd588b3
ButlerFuqua <butlerfuqua@gmail.com>
2020-09-12 20:08:26 -0400

    Add Basic static PWA and run game in it.

---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register('sw.js')
+        .then(registration => {
+            // console.log(registration)
+        })
+        .catch(error => {
+            console.log("SW Registration failed.")
+            console.log(error)
+        })
+
+} else {
+    // browser does not have a service worker
+}
