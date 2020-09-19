41a1b424025af5e5ad0ecdce855523fd8d88ea82
ButlerFuqua <butlerfuqua@gmail.com>
2020-09-19 16:46:14 -0400

    Add buttons to bottom-bar

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -8,16 +8,42 @@ export default ({ colors }) => {
         box-sizing: border-box;
     }
 
-    .container {
+    #container {
         height: 45px;
-        background: ${colors.dark1}
+        background: ${colors.dark1};
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    #container button {
+        background: ${colors.dark1};
+        border: none;
+        max-height: 100%;
+    }
+
+    #container .selectBtn {
+        height: 30px;
+    }
+
+    #menuBtn{
+        width: 45px;
+        height: inherit;
     }
 
     `
     const template = document.createElement('template')
     template.innerHTML = `
-           <div class="container">
-
+           <div id="container">
+                <button>
+                    <img class="selectBtn" id="spotsBtn" src="/src/images/default_icon.png" />
+                </button>
+                <button>
+                    <img id="menuBtn" src="/src/images/start_btn.png" />
+                </button>
+                <button>
+                    <img class="selectBtn"  id="friendsBtn" src="/src/images/default_icon.png" />
+                </button>
            </div>
         
     `
