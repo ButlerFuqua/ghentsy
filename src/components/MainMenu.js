ff5cd3cd789819ebb83194012b97084d46d1fd2b
ButlerFuqua <butlerfuqua@gmail.com>
2020-09-20 11:50:35 -0400

    Add main-menu slideDown, slideUp and remove

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -10,6 +10,35 @@ export default ({ colors }) => {
 
     #container {
         background: ${colors.dark1};
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+        z-index: 1;
+        color: white;
+        bottom: 10vh;
+        animation: slideDown .6s forwards;
+    }
+
+    #container.slideUp{
+        animation: slideUp .6s forwards;
+    }
+
+    @keyframes slideDown {
+        0%{
+            bottom: 100vh;
+        }
+        100%{
+            bottom: 0;
+        }
+    }
+
+    @keyframes slideUp {
+        0%{
+            bottom: 0v;
+        }
+        100%{
+            bottom: 100vh;
+        }
     }
 
  
@@ -19,6 +48,7 @@ export default ({ colors }) => {
     template.innerHTML = `
            <div id="container">
                This is the main menu
+               <button id="closeMenuBtn">Close menu</button>
            </div>
         
     `
@@ -32,10 +62,26 @@ export default ({ colors }) => {
         }
 
         static slideDown() {
-            console.log('show menu')
+
+            const menu = document.createElement('main-menu')
+
+            document.body.prepend(menu)
+        }
+
+        removeMainMenu() {
+            document.querySelector('main-menu').remove()
+            console.log(document.querySelector('main-menu'))
+        }
+
+        closeMenu() {
+            const menuContainer = this.shadowRoot.getElementById('container')
+            if (!menuContainer.classList.contains('slideUp')) menuContainer.classList.add('slideUp')
+            setTimeout(this.removeMainMenu, 600)
         }
 
         connectedCallback() {
+
+            this.shadowRoot.getElementById('closeMenuBtn').addEventListener('pointerdown', this.closeMenu.bind(this))
 
         }
 
